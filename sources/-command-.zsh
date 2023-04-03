67d0eb5dacbb9a4dc25c9c6fc3b609b1f705adc0
Yarden zamir <make.charcoal@gmail.com>
2023-04-03 20:20:00 +0300

    Expand command preview

--- sources/-command-.zsh
+++ sources/-command-.zsh
@@ -1,15 +1,25 @@
 # :fzf-tab:complete:(-command-:|command:option-(v|V)-rest)
+# some logic taken from https://github.com/fluxninja/dotfiles/blob/master/dot_zshrc
+
+export COLUMNS=$(($FZF_PREVIEW_COLUMNS - 2))
 case $group in
-'external command')
-  less =$word
+'[shell function]'|'[alias]')
+  (which $word &> /dev/null) || source $HOME/.zshrc
+  which $word | bat --color=always -pl zsh
   ;;
-'executable file')
-  less ${realpath#--*=}
+'[external command]'|'[executable file]'|'[builtin command]'|['builtin command'])
+  (out=$(man $word) 2>/dev/null && echo $out) ||\
+  (out=$(tldr "$word") 2>/dev/null && echo $out) ||\
+  (out=$($word --help) 2>/dev/null && echo $out) ||\
+  (out=$(which "$word") && echo $out) ||\
+  echo "$word"
   ;;
-'builtin command')
-  run-help $word | bat --color=always -plman
+'[reserved word]')
+  (out=$(tldr "$word") 2>/dev/null && echo $out) ||\
+  (out=$(man $word) 2>/dev/null && echo $out) ||\
+  echo reserved word - $word | bat --color=always -pl zsh
   ;;
-parameter)
-  echo ${(P)word}
+'[parameter]')
+  echo ${(P)word} | bat --color=always -pl zsh
   ;;
 esac
