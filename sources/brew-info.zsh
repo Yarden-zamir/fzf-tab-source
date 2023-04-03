eefd545265bc77919821ea285a315dfefed28778
Yarden zamir <make.charcoal@gmail.com>
2023-04-03 20:20:18 +0300

    Update brew-info.zsh

--- sources/brew-info.zsh
+++ sources/brew-info.zsh
@@ -1,2 +1,7 @@
-# :fzf-tab:complete:brew-((|un)install|info|cleanup):argument-rest
-brew info $word | bat --color=always -plyaml
+# :fzf-tab:complete:brew-(install|uninstall|reinstall|upgrade|pin|unpin|link|unlink|info|cat|uses|dep):*
+
+brew desc --eval-all $word | cut -d: -f2\
+&& echo "\n# brew info:" | bat --color=always --style=plain --language=markdn \
+ && brew info $word | bat --color=always --style=plain --language=markdn \
+&& echo "\n# brew log:" | bat --color=always --style=plain --language=markdn \
+ && brew log --stat --max-count 2 $word | bat --color=always --style=plain --language=markdn
